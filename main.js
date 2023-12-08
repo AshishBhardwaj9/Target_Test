b851fcb199329be9900bfa93e20f41ef9b370085
Ashish <94374788+AshishBhardwaj9@users.noreply.github.com>
2023-12-08 11:46:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// var msg = document.querySelectorAll(".message a");
+// msg.addEventListener("click", function () {
+//   $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+// });
+// .click(function () {
+//   $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+// });
+// function msgFn() {
+//   $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+// }
